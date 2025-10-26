d11d7c84aa8bbfb7a93438514e264c1fd632cafa
Dalibor Klobučarić <info@dd-lab.net>
2025-10-26 23:44:06 +0100

    Create QrRender class for PNG rendering
    Added QrRender class with method toPng for PNG conversion.

---
+++ src/QrRender.php
@@ -0,0 +1,9 @@
+<?php
+// src/QrRender.php
+// TODO: Convert matrix into colored PNG
+
+class QrRender {
+    public static function toPng(array $matrix, string $colorHex, int $sizePx) {
+        // TODO: implement drawing logic using GD
+    }
+}
