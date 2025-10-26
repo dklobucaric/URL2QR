88bacc4d2a757698ae51318a967a3ca21fb7d3eb
Dalibor Klobučarić <info@dd-lab.net>
2025-10-26 23:43:47 +0100

    Create QrMatrix class with fromText method
    Initial implementation of QrMatrix class with a static method to generate a 2D QR matrix from text.

---
+++ src/QrMatrix.php
@@ -0,0 +1,9 @@
+<?php
+// src/QrMatrix.php
+// TODO: Class that generates 2D QR matrix (0/1 array)
+class QrMatrix {
+    public static function fromText(string $text): array {
+        // TODO: integrate phpqrcode library and return 2D matrix
+        return [];
+    }
+}
